51a1b525a90723761415cd715cde8312c3795768
DeepakGupta4 <dkguptak9369.com>
2025-03-23 16:10:53 +0530

    two

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/',  // ✅ Isse /admin route ke assets sahi load honge
+  base: '/admin/', // Set the base path for your app
 });
